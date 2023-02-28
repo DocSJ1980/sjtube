1e508d99dab025c16a1dea897ca101f6386bcfcc
DocSJ1980 <docsaj@outlook.com>
2023-02-28 22:12:51 +0500

    Sidebar basic setup

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import { categories } from '../utils/Constants'
+
+const selectedCategory = 'New'
+
+const Sidebar = () => (
+    <Stack direction="row"
+        sx={{ overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { sx: 'row', md: 'column' } }}
+    >
+        {categories.map((category) => (
+            <button
+                className='category-btn'
+                style={{
+                    background: category.name === selectedCategory && '#FC1503', color: 'white'
+                }}
+                key={category.name}
+            >
+                <span
+                    style={{
+                        color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'
+                    }}
+                >{category.icon}</span>
+                <span
+                    style={{
+                        opacity: category.name === selectedCategory ? 1 : 0.8
+                    }}
+                >{category.name}</span>
+            </button>
+        ))}
+    </Stack>
+)
+
+
+export default Sidebar
