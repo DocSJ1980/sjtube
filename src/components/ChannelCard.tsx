ae1789b108f37468c8454bf5ebd87aa72fcfd0c7
DocSJ1980 <docsaj@outlook.com>
2023-03-06 18:12:08 +0500

    Channel card refactored

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -7,24 +7,44 @@ import { ChannelCardProps } from '../utils/types'
 
 
 const ChannelCard = ({ channelDetail }: ChannelCardProps) => {
+    console.log("Channel Detail:", channelDetail)
     return (
         <Box
             sx={{
                 boxShadow: 'none',
                 borderRadius: '20px',
+                width: '320px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
             }}
         >
-            <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+            <Link to={`/channel/${channelDetail?.snippet?.channelId}`}>
                 <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
                     <CardMedia
                         image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
                         // alt={channelDetail?.snippet?.title}
-                        sx={{ borderRadius: '50%', width: '180px', height: '180px' }}
+                        sx={{ borderRadius: '50%', width: '180px', height: '180px', mb: 2, border: '1px solid #e3e3e3' }}
                     />
-
                 </CardContent>
+                <Typography variant='subtitle1' fontWeight='bold' color='gray'>
+                    {channelDetail?.snippet?.title.slice(0, 60) || demoChannelTitle.slice(0, 60)}
+                </Typography>
+            </Link>
+            <Link to={`/channel/${channelDetail?.snippet?.channelId}`}>
+                <Typography variant='subtitle2' fontWeight='bold' color='gray'>
+                    {channelDetail?.snippet?.channelTitle || demoChannelTitle}
+                    <CheckCircle sx={{
+                        fontSize: '12px',
+                        color: 'gray',
+                        ml: '5px',
+                    }} />
+                </Typography>
             </Link>
         </Box>
+
     )
 }
 
